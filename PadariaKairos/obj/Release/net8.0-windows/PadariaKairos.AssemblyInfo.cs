4d2a84e56c7ed76c580ea132e950e5d2409512e8
Underquartz <underquartz@gmail.com>
2024-12-14 13:43:35 -0300

    Atualização do Projeto

--- PadariaKairos/obj/Release/net8.0-windows/PadariaKairos.AssemblyInfo.cs
+++ PadariaKairos/obj/Release/net8.0-windows/PadariaKairos.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PadariaKairos")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e50e6d620b2c4c126d15cd9cec855c2385a0e81")]
 [assembly: System.Reflection.AssemblyProductAttribute("PadariaKairos")]
